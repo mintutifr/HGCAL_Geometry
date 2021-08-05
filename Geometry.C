f3f9d52216cb11cb2449aea2031ac971a90c366c
Mintu Kumar <mintuswami05@gmail.com>
2021-08-05 17:45:15 +0530

    update

--- Geometry.C
+++ Geometry.C
@@ -23,7 +23,7 @@ void Geometry()
    //--sey top valuome to be dectro volume
    //geom->SetTopVolume(detac);
    //--define layer Volume
-   TGeoVolume *layer = geom->MakeBox("LAYER", Vacuum,30,30,1.5);
+   TGeoVolume *layer = geom->MakeBox("LAYER", Vacuum,40,40,1.5);
    //layer->SetVisibility(kFALSE);
    geom->SetTopVolume(layer);
    //--- define some materials
@@ -36,8 +36,9 @@ void Geometry()
    TGeoVolume *hexa = geom->MakePgon("HEXA", Al, 0.0,360.0,6,2);
    hexa->SetLineColor(kGreen);
    
-   //TGeoVolume *hexa1 = geom->MakePgon("HEXA1", Al, 0.0,360.0,6,2);
-   //hexa1->SetLineColor(kGreen);
+   TGeoVolume *hexa1 = geom->MakePgon("HEXA1", Al, 0.0,360.0,6,2);
+   hexa1->SetFillColor(kRed);
+   hexa1->SetLineColor(kRed);
 
    //--side of the hexagon--
    double Sqrt3 = sqrt(3.0);
@@ -48,8 +49,13 @@ void Geometry()
    TGeoPgon *pgon = (TGeoPgon*)(hexa->GetShape());
    pgon->DefineSection(0,0,0,dr);
    pgon->DefineSection(1,1,0,dr);
-   geom->SetTopVolume(hexa);
-   
+   //geom->SetTopVolume(hexa);
+
+   TGeoPgon *pgon1 = (TGeoPgon*)(hexa1->GetShape());
+   pgon1->DefineSection(0,0,0,dr);
+   pgon1->DefineSection(1,1,0,dr);
+   //geom->SetTopVolume(hexa);
+
    TGeoTranslation *tr1 = new TGeoTranslation(0., 0., 0.);
    layer->AddNode(hexa, 1, tr1);
 	
@@ -58,27 +64,32 @@ void Geometry()
    TGeoRotation   *rot1 = new TGeoRotation("rot1", 90,180,90,90,0,30);
 
    int num_crl=3;
-   TGeoCombiTrans *combi[6*num_crl];
+   TGeoTranslation *trns[6*num_crl];
    for(int crl=1;crl<=num_crl;crl++){
    	for(int i=1;i<=6;i++){
 		double angle = i*M_PI/3.0+(crl-1)*M_PI/6.0;
 		if(crl%2==0){
-		       	combi[i+6*(crl-1)] = new TGeoCombiTrans((crl/2+2)*a*sin(angle),(crl/2+2)*a*cos(angle), 0., rot1);
-			layer->AddNode(hexa, 1, combi[i+6*(crl-1)]);
+		    trns[i+6*(crl-1)] = new TGeoTranslation((crl/2+2)*a*sin(angle),(crl/2+2)*a*cos(angle), 0.);
+			layer->AddNode(hexa, 1, trns[i+6*(crl-1)]);
 		}
 		else{
-			combi[i+6*(crl-1)] = new TGeoCombiTrans((crl+1)*dr*sin(angle),(crl+1)*dr*cos(angle), 0., rot1);
-			layer->AddNode(hexa, 1, combi[i+6*(crl-1)]);
+			trns[i+6*(crl-1)] = new TGeoTranslation((crl+1)*dr*sin(angle),(crl+1)*dr*cos(angle), 0.);
+			layer->AddNode(hexa1, 1, trns[i+6*(crl-1)]);
 		}
 		//cout<<i<<endl;
    	}
    }
 
    TGeoCombiTrans *combi2[6];
-   double angle = atan(3.0*Sqrt3/8.0);
-   double R = sqrt(27.0/4.0+16.0)*dr;
-   combi2[0] = new TGeoCombiTrans(R*sin(angle),R*cos(angle),0.,rot1);//((crl/2+2)*a*sin(an),(crl/2+2)*a*cos(angle), 0., rot1);
-   layer->AddNode(hexa, 1, combi2[0]);
+   double angle = atan(Sqrt3/2);
+   double R = 2*sqrt(7.0)*dr;
+   for(int i=1;i<=6;i++){
+      combi2[i] = new TGeoCombiTrans(R*sin(angle),R*cos(angle),0.,rot1);
+      layer->AddNode(hexa, 1, combi2[i]);
+      angle+=M_PI/3.0;
+   }
+   //combi2[1] = new TGeoCombiTrans(R*sin(angle+M_PI/3.0),R*cos(angle+M_PI/3.0),0.,rot1);
+   //layer->AddNode(hexa, 1, combi2[1]);
    //TGeoTranslation *tr2 = new TGeoTranslation(20., 0, 0.);
    //detac->AddNode(layer, 1, tr2);
    geom->CloseGeometry();
